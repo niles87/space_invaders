080ce8069f1bd754a6bf2d09ee3f84393ff5170f
niles87 <nbing19@gmail.com>
2020-07-09 08:06:05 -0700

    added a background image and the players attack

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ pygame.init()
 # create screen
 screen = pygame.display.set_mode((800, 600))
 
+# background
+background = pygame.image.load(os.path.join(game_folder, 'stars_texture.png'))
+
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load(os.path.join(game_folder, 'icon.png'))
@@ -26,11 +29,28 @@ def player(x, y):
     screen.blit(playerImg, (x, y))
 
 
+# Player Attack
+bulletImg = pygame.image.load(os.path.join(game_folder, 'player_attack.png'))
+bulletX = 0
+bulletY = playerY
+bulletX_Change = 0
+bulletY_Change = 7
+# bullet states ready and fired
+bullet_state = "ready"
+
+
+def player_attack(x, y):
+    global bullet_state
+    bullet_state = "fired"
+    screen.blit(bulletImg, (x + 27, y + 5))
+
+
 # Enemy
 enemyImg = pygame.image.load(os.path.join(game_folder, 'enemy.png'))
 enemyX = random.randint(0, 800-64)
 enemyY = 20
-enemyX_Change = 0
+enemyX_Change = 2.5
+enemyY_Change = 40
 
 
 def enemy(x, y):
@@ -42,6 +62,8 @@ running = True
 while running:
 
     screen.fill((0, 15, 90))
+    # backgound image
+    screen.blit(background, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,6 +75,11 @@ while running:
                 playerX_Change -= 5
             if event.key == pygame.K_RIGHT:
                 playerX_Change += 5
+            if event.key == pygame.K_SPACE:
+                if bullet_state == "ready":
+                    # get current x coordinate
+                    bulletX = playerX
+                    player_attack(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_Change = 0
@@ -63,6 +90,23 @@ while running:
         playerX = 0
     elif playerX >= 800-64:
         playerX = 800-64
+
+    enemyX += enemyX_Change
+
+    if enemyX <= 0:
+        enemyX_Change = 2.5
+        enemyY += enemyY_Change
+    elif enemyX >= 800-64:
+        enemyX_Change = -2.5
+        enemyY += enemyY_Change
+
+    # attack movement
+    if bulletY <= 0:
+        bulletY = playerY
+        bullet_state = "ready"
+    if bullet_state == "fired":
+        player_attack(bulletX, bulletY)
+        bulletY -= bulletY_Change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
