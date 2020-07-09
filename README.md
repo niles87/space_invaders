8e2bc4a324b50f174b905af3e150ff884ee7f778
niles87 <nbing19@gmail.com>
2020-07-08 21:13:46 -0700

    added imgs and started to build display

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # space_invaders
+
+### Icons and Images
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div><div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/"     title="Flaticon">www.flaticon.com</a></div>
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pygame
+import os
+
+game_folder = os.path.dirname(__file__)
 
 # initialize game
 pygame.init()
@@ -6,9 +9,29 @@ pygame.init()
 # create screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load(os.path.join(game_folder, 'icon.png'))
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load(os.path.join(game_folder, 'player.png'))
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill((0, 15, 90))
+
+    player()
+    pygame.display.update()
