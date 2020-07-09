48031c9b06c8e836c70fdb83c762b2bfb56d3092
niles87 <nbing19@gmail.com>
2020-07-08 20:10:17 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+# initialize game
+pygame.init()
+
+# create screen
+screen = pygame.display.set_mode((800, 600))
+
+# game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
